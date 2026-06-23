51c2e246601b90c08054f28b413c01adf9e1432f
nnbalaa <bala.6985@gmail.com>
2026-06-23 19:18:44 +0530

    Fix manifest icon routes

--- app/manifest.ts
+++ app/manifest.ts
@@ -12,13 +12,8 @@ export default function manifest(): MetadataRoute.Manifest {
     theme_color: "#001f60",
     icons: [
       {
-        src: "/icon",
-        sizes: "32x32",
-        type: "image/png",
-      },
-      {
-        src: "/apple-icon",
-        sizes: "180x180",
+        src: "/icon.png",
+        sizes: "any",
         type: "image/png",
       },
     ],
